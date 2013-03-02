0ad69bb9e62135389f6f9e5064f9a16957d5535f
mtrupkin <mike.trupkin@gmail.com>
2013-03-02 13:53:41 -0800

    Setup game loop

---
+++ AutoRL/AutoGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRL
+{
+    class AutoGame
+    {
+                public bool Pause { get; set; }
+
+        public bool Complete { get; set; }
+
+        public Road Road { get; set; }
+
+        public AutoGame()
+        {
+            Pause = false;
+            Complete = false;
+        }
+
+        public int Update(int duration)
+        {
+            if (Road != null)
+            {
+                if (!Pause)
+                {
+                    return Road.Update(duration);
+                }
+            } 
+
+            return duration;
+        }
+
+    }
+}
